d9ac2e040324af3c4d72ffbc9b72c30f5132f2a0
dkhomenko <dinamastercs@gmail.com>
2025-01-03 12:59:22 +0100

    add token

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = "https://api.themoviedb.org/3";
+const API_URL = 'https://api.themoviedb.org/3';
 const API_TOKEN =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMmM5ZWQzMWY3ODBlYjdhZTM0ZWM5MzRiMjNjOTBmMCIsIm5iZiI6MTczNTQ5ODI2OC43MjcsInN1YiI6IjY3NzE5YTFjN2QxYmM4N2RlNzYxNzg5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z1mNQC7u7uhRf4YZ5jm5_gV2zVQigxEaSuCNNF5uU6k";
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzQzNThiZDc1NGE4ZDU4OGZhZTBkZGVjN2UwYmY3ZCIsIm5iZiI6MTczNDAxMDEyMy42NDQsInN1YiI6IjY3NWFlNTBiODk2NTMwZjIwYjlmNDUyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pA79sd0MFdYNH2BwtfVA_XYpzheJGQaTos9qEPa4SWI';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
@@ -11,29 +11,29 @@ const axiosInstance = axios.create({
 
 export const fetchPopularMovies = async () => {
   const response = await axiosInstance.get(
-    "/movie/popular?language=en-US&page=1"
+    '/movie/popular?language=en-US&page=1'
   );
   return response.data.results;
 };
 
-export const searchMovies = async (query) => {
+export const searchMovies = async query => {
   const response = await axiosInstance.get(
     `/search/movie?query=${query}&language=en-US&page=1`
   );
   return response.data.results;
 };
 
-export const fetchMovieDetails = async (movieId) => {
+export const fetchMovieDetails = async movieId => {
   const response = await axiosInstance.get(`/movie/${movieId}?language=en-US`);
   return response.data;
 };
 
-export const fetchMovieCast = async (movieId) => {
+export const fetchMovieCast = async movieId => {
   const response = await axiosInstance.get(`/movie/${movieId}/credits`);
   return response.data.cast;
 };
 
-export const fetchMovieReviews = async (movieId) => {
+export const fetchMovieReviews = async movieId => {
   const response = await axiosInstance.get(`/movie/${movieId}/reviews`);
   return response.data.results;
 };
